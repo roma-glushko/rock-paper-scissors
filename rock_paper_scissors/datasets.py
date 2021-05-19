🛠 fixed the training pipeline and added [-1, 1] image scaling required by MobileNetV2 model

rock_paper_scissors/datasets.py
@@ -8,8 +8,8 @@ from tensorflow.python.keras.preprocessing.image_dataset import image_dataset_fr
 
 
 def augment_image(inputs, labels, augmentation_pipeline):
-    def apply_augmentation(image):
-        aug_data = augmentation_pipeline(image=image)
+    def apply_augmentation(images):
+        aug_data = augmentation_pipeline(image=images)
         return aug_data['image']
 
     inputs = tf.numpy_function(func=apply_augmentation, inp=[inputs], Tout=tf.float32)
@@ -36,12 +36,14 @@ def get_dataset(
         subset=subset_type,
         validation_split=validation_fraction,
         image_size=image_size,
+        batch_size=batch_size,
         seed=seed,
     )
 
+    # https://www.tensorflow.org/tutorials/images/transfer_learning
+
     return dataset \
         .map(augmentation_func, num_parallel_calls=AUTOTUNE) \
-        .batch(batch_size) \
         .prefetch(AUTOTUNE)
 
 
@@ -59,11 +61,11 @@ def get_test_dataset(
 
     dataset = image_dataset_from_directory(
         dataset_path,
+        batch_size=batch_size,
         image_size=image_size,
         seed=seed,
     )
 
     return dataset \
         .map(augmentation_func, num_parallel_calls=AUTOTUNE) \
-        .batch(batch_size) \
         .prefetch(AUTOTUNE)

train.py
@@ -2,19 +2,18 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
-from tensorflow.keras.applications import MobileNetV2
 
 from morty.config import ConfigManager, main
 from morty.experiment import set_random_seed
 
-# TF setup
-from rock_paper_scissors import get_dataset
+from rock_paper_scissors import get_dataset, get_model
 
+# TF setup
 tf.get_logger().setLevel('ERROR')
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
-print('TF', tf.__version__)
+print('TF:', tf.__version__)
 print('Num GPUs Available: ', len(tf.config.list_physical_devices('GPU')))
 
 
@@ -42,16 +41,37 @@ def train(config: ConfigManager) -> None:
         seed=config.seed,
     )
 
-    mobile_netv2 = MobileNetV2(
-        input_shape=(*config.image_size, 3),
-        include_top=False,
-        weights='imagenet',
-        pooling='avg'
+    model = get_model(
+        config.feature_extractor,
+        config.num_classes,
+        config.image_size,
     )
 
-    mobile_netv2.trainable = False
+    optimizer = tf.keras.optimizers.Adam(learning_rate=config.learning_rate)
 
-    mobile_netv2.summary()
+    model.compile(
+        optimizer=optimizer,
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'],
+    )
+
+    model.summary()
+
+    # todo: find a way to get dataset lengths
+    steps_per_epoch = 2016 // config.batch_size
+    validation_steps = 504 // config.batch_size
+
+    training_history = model.fit(
+        x=train_dataset.repeat(),
+        validation_data=validation_dataset.repeat(),
+        epochs=config.epochs,
+        steps_per_epoch=steps_per_epoch,
+        validation_steps=validation_steps,
+        callbacks=[
+            # model_checkpoint_callback,
+        ],
+        verbose=1
+    )
 
 
 if __name__ == "__main__":